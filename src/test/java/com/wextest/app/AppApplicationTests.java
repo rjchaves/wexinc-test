bf3a169f00fa78c654dedfebc69e4683a6be7862
Raul José Chaves <raul.chaves@proton.me>
2023-10-21 14:01:18 -0300

    Initial commit

---
+++ src/test/java/com/wextest/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wextest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
